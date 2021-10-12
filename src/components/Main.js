f71f8511d1a12fe574e1d8ada2a526c2d733b44c
ILYA <den4592@naver.com>
2021-10-12 17:25:28 +0900

    영상 compress

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import wave_video from "../video/wave-video.mp4";
+import wave_video from "../video/compress.mp4";
 
 const Container = styled.div`
   width: 100%;
